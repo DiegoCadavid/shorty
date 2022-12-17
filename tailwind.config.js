dd335dde0a58a5b3dbd21c1f7eae7aa272b19e10
DiegoCadavid <dcmgames50@gmail.com>
2022-12-17 15:09:31 -0500

    feat: add basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        page: "600px 1fr",
+      },
+      colors: {
+        primary: {
+          50: "#ceeceb",
+          100: "#9BDEDB",
+          200: "#68CDC9",
+          300: "#00B2A9",
+          400: "#00A499",
+          500: "#636363",
+        },
+        secondary : {
+          50: "#b084e6",
+          100: "#916FDD",
+          200: "#8294f2",
+          300: "#6D3FD1",
+          400: "#480FC6",
+          500: "#331F68"
+        }
+      },
+    },
+    fontFamily: {
+      "primary": ["Acumin Pro ExtraCondensed", "sans serif"],
+      "secondary": ["Proxima Nova", "sans serif"]
+    }
   },
   plugins: [],
-}
+};
