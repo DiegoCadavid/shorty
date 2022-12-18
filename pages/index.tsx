8155d6593a4da495fc6814dd5a829e27e1954586
DiegoCadavid <dcmgames50@gmail.com>
2022-12-17 19:11:44 -0500

    feat: add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image";
+import ShortyInfo from "../components/shortyInfo/ShortyInfo";
+import ShortyRegister from "../components/shortyRegister/ShortyRegister";
+
 export default function Home() {
   return (
-    <>
-    </>
-  )
+    <div className="lg:grid lg:grid-cols-page min-h-screen">
+      <ShortyInfo />
+      <ShortyRegister />
+    </div>
+  );
 }
