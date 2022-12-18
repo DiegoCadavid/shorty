00bb52a8dc8b0726897d94d62e2f8455c25714a3
DiegoCadavid <dcmgames50@gmail.com>
2022-12-17 20:21:57 -0500

    refactor: Edit readme

--- components/shortyInfo/ShortyInfoNav.tsx
+++ components/shortyInfo/ShortyInfoNav.tsx
@@ -26,7 +26,6 @@ const links: ILink[] = [
 
 const ShortyInfoNav = ({}: Props) => {
   return (
-    // lg:text 27px -> 18px & 
     <nav className="text-white font-semibold text-ssm lg:text-lg | flex mt-11 justify-between px-4 lg:mt-0 lg:px-0 lg:flex-col lg:items-end lg:gap-2.5 ">
       {links.map((link) => {
         return (
