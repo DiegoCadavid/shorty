c72816092e0a60af08a78995e991ef475babdbdd
DiegoCadavid <dcmgames50@gmail.com>
2022-12-17 20:10:06 -0500

    feat: add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
+import Head from "next/head";
 import Image from "next/image";
 import ShortyInfo from "../components/shortyInfo/ShortyInfo";
 import ShortyRegister from "../components/shortyRegister/ShortyRegister";
 
 export default function Home() {
   return (
-    <div className="lg:grid lg:grid-cols-page min-h-screen">
-      <ShortyInfo />
-      <ShortyRegister />
-    </div>
+    <>
+      <Head>
+        <title> Shorty Awards | Register </title>
+        <link  rel="icon"   href="favicon.png" type="image/png" />
+      </Head>
+      <main className="lg:grid lg:grid-cols-page min-h-screen">
+        <ShortyInfo />
+        <ShortyRegister />
+      </main>
+    </>
   );
 }
