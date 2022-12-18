b25b0fa34c63955b8bc22bb21388f35ca5d520d6
DiegoCadavid <dcmgames50@gmail.com>
2022-12-17 19:11:22 -0500

    refactor: add config styles in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,20 +18,36 @@ module.exports = {
           400: "#00A499",
           500: "#636363",
         },
-        secondary : {
+        secondary: {
           50: "#b084e6",
           100: "#916FDD",
           200: "#8294f2",
           300: "#6D3FD1",
           400: "#480FC6",
-          500: "#331F68"
-        }
-      }, 
+          500: "#331F68",
+        },
+        muted: "#848484",
+        brd: "#EBEBEB"
+      },
+
+      fontFamily: {
+        primary: ["Acumin Pro", "sans serif"],
+        secondary: ["Proxima Nova", "sans serif"],
+      },
+      backgroundImage: {
+        shorty: "url(/images/shorty.jpg)",
+      },
+      padding: {
+        "4.2" : "18px"
+      },
+      fontSize: {
+        "ssm" : ["13px", "22.26px"],
+        "2.2xl": ["27px", "46.27px"]
+      },
+      spacing: {
+        "dropdown-icon" : "calc(50% - 9px)"
+      }
     },
-    fontFamily: {
-      "primary": ["Acumin Pro", "sans serif"],
-      "secondary": ["Proxima Nova", "sans serif"]
-    }
   },
   plugins: [],
 };
